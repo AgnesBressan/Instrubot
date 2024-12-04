ddc284f6ba4f9bdf32f9e1a2430a186b17217ea7
AgnesBressan <agnesbressan3838@gmail.com>
2024-12-04 04:55:49 -0300

    criação do ambiente simulado de uma cirurgia

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,7 +8,7 @@ else
 fi
 cd ~/catkin_ws/src/
 cd instru_bot
-cd ~/catkin_ws/src/instru_bot/models && cp -r ./mesh ./doctor ~/.gazebo/models/
+cd ~/catkin_ws/src/instru_bot/models && cp -r ./mesh ./doctor ./chao ./bed ./shelf ./parede ./anest ./small_cabinet ./big_cabinet ./monitor ./camera_sensor ./micro ~/.gazebo/models/
 cd ..
 cd ..
 cd ..
